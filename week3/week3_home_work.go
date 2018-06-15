f420619a04f082ec4299fb61acd8a6620e220da0
wesley_su <wesley_su@mail.chungyo.net>
2018-06-15 10:54:03 +0800

    Week3 作業

---
+++ week3/week3_home_work.go
@@ -0,0 +1,40 @@
+package main
+
+func main()  {
+	Q1()
+	Q2()
+}
+
+func Q1()  {
+
+}
+
+func Q2()  {
+	/* Q2-1
+	IP:192.17.55.3	Host:docs.google.com 		Port:80
+	IP:192.17.33.17 Host:ts-phpadmin.vir999.com Port:80
+	IP:192.17.99.52 Host:jsonviewer.stack.hu 	Port:7711
+	*/
+
+
+
+	/* Q2-2
+	IP:177.18.2.33 Host:github.com Port:80
+	*/
+
+
+
+	// Q2-3
+	//updateSetting
+
+}
+
+func updateSetting() {
+
+}
+
+func getApiResult() []byte {
+	userData := `{"ret":[{"id":148500286,"parent_id":1414663,"parent":1414663,"all_parents":[1414663,59735,58971,24367,6],"domain":6,"last_login":"2018-06-12T09:17:51+0800","currency":"CNY","password_expire_at":"2020-09-04T18:19:02+0800","password_reset":false,"last_bank":54,"username":"wesley01","block":false,"bankrupt":false,"cash":{"id":71814701,"user_id":148500286,"balance":4923642.0545,"pre_sub":0,"pre_add":0,"currency":"CNY","last_entry_at":"20180612155900"},"cash_fake":null,"card":null,"enabled_card":null}],"result":"ok","profile":{"execution_time":14,"server_name":"ipl-web01.rd5.qa"}}`
+
+	return []byte(userData)
+}
